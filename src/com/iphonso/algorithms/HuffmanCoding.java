382406be26e3172b19e6c5712af81a656d802e8f
Sebastian Vieira <vieira@mail.com>
2013-02-11 22:19:59 +0100

    Huffman coding algorithm fixed

--- src/com/iphonso/algorithms/HuffmanCoding.java
+++ src/com/iphonso/algorithms/HuffmanCoding.java
@@ -2,8 +2,8 @@ package com.iphonso.algorithms;
 
 import java.io.IOException;
 import java.util.*;
+import com.iphonso.algorithms.io.*;
 
-import com.iphonso.algorithms.io.BitOutputStream;
 /*
  * todo: intermediate nodes are also part of the encoding table and have to be considered (and put in the queue when discovered)
  */
@@ -93,6 +93,24 @@ public class HuffmanCoding {
 		return ret.toString();
 	}
 	
+	private void buildHuffmanCodingTable(Node tree, int val, int length) {
+		if (tree == null) {
+			return;
+		}
+		if (!tree.isIntermediateNode) {
+			mEncodingTable[tree.c] = val;
+			mEncodingLengthTable[tree.c] = length;
+		}
+
+		Node n0 = tree.child0;
+		Node n1 = tree.child1;
+		length++;
+		val = val << 1;
+		buildHuffmanCodingTable(n0, val, length);
+		val = val | 1;
+		buildHuffmanCodingTable(n1, val, length);
+	}
+
 	public HuffmanCodingData encode(String e) throws IOException {
 		
 		System.out.println("Encode " + e.length() + "bytes");
@@ -120,56 +138,39 @@ public class HuffmanCoding {
 			return null;
 		}
 		
-		Node tree;
+		Node tree = null;
 		if (nodes.size() == 1) {
 			tree = nodes.remove();
 		} else {
-			Node n0 = nodes.remove();
-			Node n1 = nodes.remove();
-			tree = new Node(n0, n1);
 			while (!nodes.isEmpty()) {
-				Node n = nodes.remove();
-				tree = new Node(n, tree);
+				Node n0 = nodes.remove();
+				if (!nodes.isEmpty()) {
+					Node n1 = nodes.remove();
+					tree = new Node(n0, n1);
+					nodes.add(tree);
+				} else {
+					tree = n0;
+				}
 			}
 		}
 		
-		// Now we have the encoding tree, lets make the lookup table;
-		int val = 0;
-		int length = 0;
 		// It is probably not a good idea to get rid of the tree
 		mTree = tree; // lets copy it (we want to decode sometimes)
-		while (tree != null) {
-			Node n0 = tree.child0;
-			Node n1 = tree.child1;
-			
-			length++;
-			val = val << 1;
-			
-			if (n0 != null && n0.weight > 0) {
-				mEncodingTable[n0.c] = val;
-				mEncodingLengthTable[n0.c] = length;
-			}
-			val = val | 1;
-			if (n1 != null && n1.weight > 0) {
-				mEncodingTable[n1.c] = val;
-				mEncodingLengthTable[n1.c] = length;
-			}
-			tree = n1;
-		}
-		
+		buildHuffmanCodingTable(tree, 0, 0);
+
 		// Lets print the lookup table values for posterity
-		//		for (int i = 0; i < 255; i++) {
-		//			if (encodingTable[i] >= 0) {
-		//				System.out.println("Char " + (char)i + " val " + Integer.toBinaryString(encodingTable[i]) + " length " + encodingLengthTable[i]);
-		//			}
-		//		}
+		for (int i = 0; i < 255; i++) {
+			if (mEncodingTable[i] >= 0) {
+				System.out.println("Char " + (char)i + " val " + Integer.toBinaryString(mEncodingTable[i]) + " length " + mEncodingTable[i]);
+			}
+		}
 		
 		// We use the lookup table to encode the whole thing
 		BitOutputStream bos = new BitOutputStream();
 		for (int i = 0; i < e.length(); i++) {
 			char c = e.charAt(i);
 			int encoded = mEncodingTable[c] ;
-			length = mEncodingLengthTable[c] ;
+			int length = mEncodingLengthTable[c] ;
 			bos.write(encoded, length);
 		}
 		bos.close();
